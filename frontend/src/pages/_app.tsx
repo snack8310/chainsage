f3f239cf50839e138b05105265e5169c749591b5
huisheng <huisheng@wacai.com>
2025-05-13 18:26:01 +0800

    修改login页面的样式

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,9 +4,16 @@ import '@radix-ui/themes/styles.css';
 import '../styles/globals.css';
 import { AuthProvider, useAuth } from '../contexts/AuthContext';
 import MainLayout from '../components/layout/MainLayout';
+import { useRouter } from 'next/router';
 
 function AppContent({ Component, pageProps }: AppProps) {
   const { username, logout } = useAuth();
+  const router = useRouter();
+  
+  // 如果是登录页面，不包装 MainLayout
+  if (router.pathname === '/login') {
+    return <Component {...pageProps} />;
+  }
   
   return (
     <MainLayout username={username} onLogout={logout}>
